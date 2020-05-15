dec522e3c75b7b9d5c60b162e89d7833cc771104
lucianosch <luciano.schiavone@ittgiorgi.edu.it>
2020-05-15 14:13:29 +0200

    Aggiunto metodo main()

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ class HelloWorld{
 	/*
 	 * Aggiungere una riga che stampi il tuo nome e cognome
 	*/
-	System.out.println("Hello, world. My name is:");
-	//Aggiungere QUI il comando che visualizza il tuo nome e cognome
-	
+	public static void main(String args[]){
+		System.out.println("Hello, world. My name is:");
+		//Aggiungere QUI il comando che visualizza il tuo nome e cognome
+	}
 }

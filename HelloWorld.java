1cdbea179bf5323513029c7b94e04f3f7a8068d6
Luciano Schiavone <luciano.schiavone@gmail.com>
2020-05-15 12:21:28 +0200

    Commit iniziale

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{
+	/*
+	 * Aggiungere una riga che stampi il tuo nome e cognome
+	*/
+	System.out.println("Hello, world. My name is:");
+	//Aggiungere QUI il comando che visualizza il tuo nome e cognome
+	
+}
